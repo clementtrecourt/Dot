af9bc9980b47446405002c3b838f926c7a310a1b
clementtrecourt <clementt.pro@protonmail.com>
2026-07-23 14:05:45 +0200

    1

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,35 +1,8 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- Surligner brièvement le texte copié (feedback visuel)
-autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ timeout = 200 })
-  end,
-})
-
--- Retour à la dernière position connue quand on rouvre un fichier
-autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- Indentation 4 espaces pour Python (PEP8), 2 pour tout le reste
-autocmd("FileType", {
-  pattern = { "python" },
-  callback = function()
-    vim.opt_local.tabstop    = 4
-    vim.opt_local.shiftwidth = 4
-  end,
-})
-
--- Désactiver l'auto-insertion de commentaires sur nouvelle ligne
-autocmd("BufEnter", {
-  callback = function()
-    vim.opt.formatoptions:remove({ "c", "r", "o" })
-  end,
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
