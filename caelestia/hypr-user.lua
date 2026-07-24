cab9c1a24aba215ce6b7e4b42f7dca173349099c
clementtrecourt <clementt.pro@protonmail.com>
2026-07-24 09:41:27 +0200

    caelestia

--- caelestia/hypr-user.lua
+++ caelestia/hypr-user.lua
@@ -1,2 +1,9 @@
-require("monitors")
+local hostname = io.popen("hostname"):read("*l")
+
+if hostname == "home" then
+    require("monitors")
+else
+    require("monitors-work")
+end
+
 hl.exec_cmd("hyprshade auto")
